a5e2ed9f355b9209f68bd83c8edfc9c6d427fe3d
AnthonyLegionLaptop <anthonyjtyip@gmail.com>
2020-12-18 00:29:59 -0800

    add files

---
+++ app.js
@@ -0,0 +1,47 @@
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const searchRouter = require('./routes/search');
+
+const app = express();
+
+// Setup mongoose connection
+const mongoose = require('mongoose')
+const dev_db_url = 'mongodb+srv://Testuser1:123123123@cluster0.tbbjq.mongodb.net/symptoStreet?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/search', searchRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
